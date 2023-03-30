0b33cc07373621b536959d51ba0882701d9c9612
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-03-30 18:22:43 +0700

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ biaya_gfrent = int(os.environ.get("BIAYA_GFRENT", "40"))
 biaya_bfrent = int(os.environ.get("BIAYA_BFRENT", "30"))
 # =========================================================== #
 
-hastag = os.environ.get("HASTAG", "#Boy #Girl #Ask #Find #Spill #Story").replace(" ", "|").lower()
+hastag = os.environ.get("HASTAG", "#Boy", "#Girl", "#Ask", "#Find", "#Spill", "#Story").replace(" ", "|").lower()
 # =========================================================== #
 
 pesan_join = os.environ.get("PESAN_JOIN", "Tidak dapat diakses harap join terlebih dahulu")
