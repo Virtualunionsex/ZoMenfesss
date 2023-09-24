57bc1494410c85b37696d7a97a99568cbb8593bf
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-09-24 17:24:37 +0700

    Update awal_handler.py

--- plugins/command/awal_handler.py
+++ plugins/command/awal_handler.py
@@ -96,8 +96,8 @@ async def gagal_kirim_handler(client: Client, msg: types.Message):
 async def help_handler(client, msg):
     db = Database(msg.from_user.id)
     member = db.get_data_pelanggan()
-    pesan = "Bagi anda yang ingin mencari fwb silahkan kirim menfess, dengan Hastagn\n"
-    pesan += '#boy ataw #girl (isi pesan)\n\nby @Carifwbindo'
+    pesan = "Silahkan kirim pesan anda di bot Mutualan instagram.\n"
+    pesan += 'Wajib menyertakan Hashtag #boy ataw #girl (isi pesan)\n\nby @MutualanIg_XBot'
     if member.status == 'admin':
         pesan += '\nHanya Admin\n'
         pesan += '/tf_coin — transfer coin\n'
